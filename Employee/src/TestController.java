e6c21d138f99f1dfbeb7ed8b0e3648dff8a2894b
AMAN ANAND <84987273+aman-anand392000@users.noreply.github.com>
2022-12-09 13:12:27 +0530

    Create TestController.java

---
+++ Employee/src/TestController.java
@@ -0,0 +1,82 @@
+package com.ems.employee;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.ems.employee.controller.EmployeeController;
+import com.ems.employee.entity.Employee;
+import com.ems.employee.service.EmployeeService;
+
+@ExtendWith(MockitoExtension.class)
+public class  ControllerTest{
+	
+	@InjectMocks
+    EmployeeController employeeController;
+
+    @Mock
+    EmployeeService employeeService;
+   
+	   @Test
+	   public void getAllEmployeesControllerTest() {
+		   List<Employee> employees = new ArrayList<>(Arrays.asList(new Employee(1001, "Ayush", "Kumar", 31, 3600),new Employee(1008, "Pradeep", "Sankar", 34, 3500)));
+	
+	       when(employeeService.getAllEmployees()).thenReturn(employees);
+	
+	       List<Employee> result = employeeController.getAllEmployees();
+	
+	       assertThat(result.size()).isEqualTo(2);
+	       assertThat(result.get(0).getFirstName()).isEqualTo(employees.get(0).getFirstName());
+	       assertThat(result.get(1).getFirstName()).isEqualTo(employees.get(1).getFirstName());
+	   }
+   
+	@Test
+	   public void addEmployeeTest()
+	   {
+	       MockHttpServletRequest request = new MockHttpServletRequest();
+	       RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+	
+	       when(employeeService.addEmployee(any(Employee.class))).thenReturn(new Employee(1001, "Aman", "Anand", 24, 3600));
+	       ResponseEntity<Void> responseEntity = employeeController.addEmployee(new Employee(1001, "Aman", "Anand", 24, 3600));
+	       assertThat(responseEntity.getStatusCode().value()).isEqualTo(201);
+	       assertThat(responseEntity.getHeaders().getLocation().getPath()).isEqualTo("/911");
+	   }
+	
+	@Test
+	public void updateEmployeeTest()
+	{
+		 MockHttpServletRequest request = new MockHttpServletRequest();
+	       RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+	       when(employeeService.updateEmployee(any(Employee.class))).thenReturn(new Employee(1001, "Ayush", "Kumar", 32, 3600));
+	       ResponseEntity<Void> responseEntity = employeeController.updateEmployee(new Employee(1001, "Ayush", "Kumar", 32, 3600));
+	       assertThat(responseEntity.getStatusCode().value()).isEqualTo(200);
+	}
+	
+	@Test
+	public void deleteEmployeeTest()
+	{
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+		long empId = 999l;
+		employeeService.removeEmployee(empId);
+		verify(employeeService,times(1)).removeEmployee(empId);
+		ResponseEntity<Void> responseEntity = employeeController.deleteEmployee(empId);
+		assertThat(responseEntity.getStatusCode().value()).isEqualTo(204);
+	}
+    
+}
