a67a4e80cab96338fdf4db82234e566ac192fc3b
AMAN ANAND <84987273+aman-anand392000@users.noreply.github.com>
2022-12-15 19:41:17 +0530

    Update EmployeeServiceImpl.java

--- Employee/src/main/java/com/sam/springjpa/service/EmployeeServiceImpl.java
+++ Employee/src/main/java/com/sam/springjpa/service/EmployeeServiceImpl.java
@@ -1,44 +1,13 @@
-package com.sam.springjpa.service;
+package com.ems.employee;
 
-import java.util.Optional;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+@SpringBootApplication
+public class EmployeeManagementSystemApplication {
 
-import com.sam.springjpa.entity.Employee;
-import com.sam.springjpa.repository.EmployeeRepository;
-
-@Service
-public class EmployeeServiceImpl implements EmployeeService {
-	@Autowired
-	EmployeeRepository empr;
-	
-	@Override
-	public Iterable<Employee> readAll() {
-		// TODO Auto-generated method stub
-		return empr.findAll();
-	}
-	
-	@Override
-	public Employee store(Employee emp) {
-		// TODO Auto-generated method stub
-		return empr.save(emp);
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
 
-	@Override
-	public void remove(Long id,Employee emp) {
-		// TODO Auto-generated method stub
-		emp=empr.findById(id).get();
-		empr.delete(emp);
-		
-	}
-
-	@Override
-	public Employee update(Long id,String fn) {
-		// TODO Auto-generated method stub
-		Employee em=empr.findById(id).get();
-		em.setFirstName(fn);
-		return empr.save(em);
-	}
-	
 }
