e149f8942ef3b78caa63a937090c0c1c978fe374
AMAN ANAND <84987273+aman-anand392000@users.noreply.github.com>
2022-12-15 19:37:06 +0530

    Update EmployeeRepository.java

--- Employee/src/main/java/com/sam/springjpa/repository/EmployeeRepository.java
+++ Employee/src/main/java/com/sam/springjpa/repository/EmployeeRepository.java
@@ -1,9 +1,11 @@
-package com.sam.springjpa.repository;
+package com.ems.employee.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import com.sam.springjpa.entity.Employee;
+import com.ems.employee.entity.Employee;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 	
 }
